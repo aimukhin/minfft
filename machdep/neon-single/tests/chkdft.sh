a9a346534f335156325945e249e6a22e0c8c191c
Alexander Mukhin <alexander.i.mukhin@gmail.com>
2019-11-05 17:06:20 +0300

    Updated paths in neon-single version of chkdft.sh

--- machdep/neon-single/tests/chkdft.sh
+++ machdep/neon-single/tests/chkdft.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+# Path to minfft library
+minfft=../../..
 # Compiler flags
-cflags="-std=c99 -pedantic -Wall -lm -L.. -lminfft"
+cflags="-std=c99 -pedantic -Wall -Wextra -lm -lminfft -L$minfft -I$minfft"
 cflags_fftw=$(echo -I ~/build/fftw-neon-vfpv3/include/ -L ~/build/fftw-neon-vfpv3/lib/ -DFFTW_PFX=fftwf -lfftw3f)
 cflags_ne10=$(echo -std=gnu17 -I ~/build/Ne10/inc/ -L ~/build/Ne10/modules/ -lNE10)
 
@@ -10,7 +12,7 @@ do_tests() {
 	t=$1
 	for d in $2; do
 		for x in $3; do
-			cc -D$t -D$d -D$x chkdft.c $cflags $4
+			cc -D$t -D$d -D$x $minfft/tests/chkdft.c $cflags $4
 			echo "# $t $d $x"
 			./a.out
 		done
