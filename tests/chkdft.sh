3004b27d5d0316daaeb5e07f3c577bb268329ed9
Alexander Mukhin <alexander.i.mukhin@gmail.com>
2019-11-07 17:23:46 +0300

    chkdft.sh: removed ALL target

--- tests/chkdft.sh
+++ tests/chkdft.sh
@@ -21,20 +21,20 @@ do_tests() {
 }
 
 if [ $# -ne 1 ]; then
-	echo "Usage: $0 <test|ALL>"
+	echo "Usage: $0 test"
 	exit 1
 fi
 t=$1
 
 # Tests of the library itself
 ## Inversion
-if [ $t = INV -o $t = ALL ]; then
+if [ $t = INV ]; then
 	dims="D1 D2 D3"
 	xforms="DFT REALDFT DCT2 DCT3 DCT4 DST4"
 	do_tests INV "$dims" "$xforms"
 fi
 ## Performance
-if [ $t = PERF -o $t = ALL ]; then
+if [ $t = PERF ]; then
 	dims="D1 D2 D3"
 	xforms="DFT INVDFT REALDFT INVREALDFT DCT2 DST2 DCT3 DST3 DCT4 DST4"
 	do_tests PERF "$dims" "$xforms"
@@ -42,13 +42,13 @@ fi
 
 # Comparisons with FFTW
 ## Accuracy
-if [ $t = CMP_FFTW -o $t = ALL ]; then
+if [ $t = CMP_FFTW ]; then
 	dims="D1 D2 D3"
 	xforms="DFT INVDFT REALDFT INVREALDFT DCT2 DST2 DCT3 DST3 DCT4 DST4"
 	do_tests CMP_FFTW "$dims" "$xforms" "$cflags_fftw"
 fi
 ## Performance
-if [ $t = PERF_FFTW -o $t = ALL ]; then
+if [ $t = PERF_FFTW ]; then
 	dims="D1 D2 D3"
 	xforms="DFT INVDFT REALDFT INVREALDFT DCT2 DST2 DCT3 DST3 DCT4 DST4"
 	do_tests PERF_FFTW "$dims" "$xforms" "$cflags_fftw"
@@ -56,13 +56,13 @@ fi
 
 # Comparisons with Kiss FFT
 ## Accuracy
-if [ $t = CMP_KISS -o $t = ALL ]; then
+if [ $t = CMP_KISS ]; then
 	dims="D1 D2 D3"
 	xforms="DFT INVDFT REALDFT INVREALDFT"
 	do_tests CMP_KISS "$dims" "$xforms" "$cflags_kiss"
 fi
 ## Performance
-if [ $t = PERF_KISS -o $t = ALL ]; then
+if [ $t = PERF_KISS ]; then
 	dims="D1 D2 D3"
 	xforms="DFT INVDFT REALDFT INVREALDFT"
 	do_tests PERF_KISS "$dims" "$xforms" "$cflags_kiss"
@@ -70,20 +70,20 @@ fi
 
 # Comparisons with Ne10
 ## Accuracy
-if [ $t = CMP_NE10 -o $t = ALL ]; then
+if [ $t = CMP_NE10 ]; then
 	dims="D1"
 	xforms="DFT INVDFT REALDFT INVREALDFT"
 	do_tests CMP_NE10 "$dims" "$xforms" "$cflags_ne10"
 fi
 ## Performance
-if [ $t = PERF_NE10 -o $t = ALL ]; then
+if [ $t = PERF_NE10 ]; then
 	dims="D1"
 	xforms="DFT INVDFT REALDFT INVREALDFT"
 	do_tests PERF_NE10 "$dims" "$xforms" "$cflags_ne10"
 fi
 
 # Test of Fortran interface (by comparison with FFTW)
-if [ $t = FORTRAN -o $t = ALL ]; then
+if [ $t = FORTRAN ]; then
 	# Compile interface modules
 	gfortran -fsyntax-only ../minfft.f03
 	gfortran -fsyntax-only fftw3.f03
