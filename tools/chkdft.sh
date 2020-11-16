d80834f2911db2023ebff0656f725a197e0a5f26
Alexander Mukhin <alexander.i.mukhin@gmail.com>
2020-11-16 17:51:45 +0300

    chkdft.sh: use -DMINFFT and minfft.o

--- tools/chkdft.sh
+++ tools/chkdft.sh
@@ -4,24 +4,30 @@
 
 # init
 if [ $# -ne 1 -a $# -ne 2 ]; then
-	echo "Usage: $0 test [fftw-suffix]"
+	echo "Usage: $0 test [SINGLE|EXTENDED]"
 	exit 1
 fi
 t=$1
-FFTW_SFX=$2
+if [ "$2" = SINGLE ]; then
+	FFTW_SFX=f
+elif [ "$2" = EXTENDED ]; then
+	FFTW_SFX=l
+fi
 
 # set compiler flags
 # common
-cflags="-std=c99 -pedantic -Wall -Wextra -lm -lminfft -I.. -L.."
+cflags="-std=c99 -pedantic -Wall -Wextra -lm"
+# for minfft
+cflags_minfft="-DMINFFT_$2 -I.. ./minfft.o"
 # for FFTW
-cflags_fftw="-DFFTW_SFX=$FFTW_SFX -lfftw3$FFTW_SFX"
+cflags_fftw="$(echo -I ~/build/fftw-mi/include -L ~/build/fftw-mi/lib -DFFTW_SFX=$FFTW_SFX -lfftw3$FFTW_SFX)"
 
 # compile and run tests
 do_tests() {
 	t=$1
 	for d in $2; do
 		for x in $3; do
-			cc -D$t -D$d -D$x chkdft.c $cflags $4
+			cc -D$t -D$d -D$x chkdft.c $cflags $cflags_minfft $4
 			if [ $? -ne 0 ]; then
 				exit
 			fi
@@ -31,6 +37,11 @@ do_tests() {
 	done
 	rm ./a.out
 }
+
+# build minfft
+echo Building minfft...
+cc $cflags -DMINFFT_$2 -c ../minfft.c
+echo done.
 
 # tests of the library itself
 ## inversion
@@ -63,18 +74,17 @@ fi
 # test of Fortran interface (by comparison with FFTW)
 if [ $t = FORTRAN ]; then
 	# Compile interface modules
-	gfortran -fsyntax-only ../minfft.f03
-	gfortran -fsyntax-only fftw3.f03
+	gfortran -fsyntax-only -DMINFFT_$2 ../minfft.F03
+	gfortran -fsyntax-only -I ~/build/fftw-mi/include fftw.f03
 	# Run tests
 	dims="D1 D2 D3"
 	xforms="DFT INVDFT REALDFT INVREALDFT DCT2 DST2 DCT3 DST3 DCT4 DST4"
 	cppflags="-DFFTW_SFX=$FFTW_SFX"
-	fflags="-lfftw3$FFTW_SFX"
 	for d in $dims; do
 		for x in $xforms; do
 			cpp -P -D$d -D$x $cppflags chkdft.F95 -o chkdft.f95 \
 			&& \
-			gfortran chkdft.f95 -L.. -lminfft $fflags
+			gfortran chkdft.f95 $cflags_minfft $cflags_fftw
 			if [ $? -ne 0 ]; then
 				exit
 			fi
@@ -82,5 +92,5 @@ if [ $t = FORTRAN ]; then
 			./a.out
 		done
 	done
-	rm ./a.out chkdft.f95 minfft.mod fftw3.mod
+	rm ./a.out chkdft.f95 minfft.mod fftw.mod
 fi
