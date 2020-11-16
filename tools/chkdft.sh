2d12f88877d3e144fdcaf0c6d7bc2f2499d2f883
Alexander Mukhin <alexander.i.mukhin@gmail.com>
2020-11-16 17:05:56 +0300

    tools: removed tests and comparisons with KissFFT and Ne10

--- tools/chkdft.sh
+++ tools/chkdft.sh
@@ -15,10 +15,6 @@ FFTW_SFX=$2
 cflags="-std=c99 -pedantic -Wall -Wextra -lm -lminfft -I.. -L.."
 # for FFTW
 cflags_fftw="-DFFTW_SFX=$FFTW_SFX -lfftw3$FFTW_SFX"
-# for KissFFT
-cflags_kiss=$(echo -I ~/build/kiss_fft/ ~/build/kiss_fft/kiss_fft*.o)
-# for Ne10
-cflags_ne10=$(echo -std=gnu17 -I ~/build/Ne10/inc/ -L ~/build/Ne10/modules/ -lNE10)
 
 # compile and run tests
 do_tests() {
@@ -62,34 +58,6 @@ if [ $t = PERF_FFTW ]; then
 	dims="D1 D2 D3"
 	xforms="DFT INVDFT REALDFT INVREALDFT DCT2 DST2 DCT3 DST3 DCT4 DST4"
 	do_tests PERF_FFTW "$dims" "$xforms" "$cflags_fftw"
-fi
-
-# comparisons with Kiss FFT
-## accuracy
-if [ $t = CMP_KISS ]; then
-	dims="D1 D2 D3"
-	xforms="DFT INVDFT REALDFT INVREALDFT"
-	do_tests CMP_KISS "$dims" "$xforms" "$cflags_kiss"
-fi
-## performance
-if [ $t = PERF_KISS ]; then
-	dims="D1 D2 D3"
-	xforms="DFT INVDFT REALDFT INVREALDFT"
-	do_tests PERF_KISS "$dims" "$xforms" "$cflags_kiss"
-fi
-
-# comparisons with Ne10
-## accuracy
-if [ $t = CMP_NE10 ]; then
-	dims="D1"
-	xforms="DFT INVDFT REALDFT INVREALDFT"
-	do_tests CMP_NE10 "$dims" "$xforms" "$cflags_ne10"
-fi
-## performance
-if [ $t = PERF_NE10 ]; then
-	dims="D1"
-	xforms="DFT INVDFT REALDFT INVREALDFT"
-	do_tests PERF_NE10 "$dims" "$xforms" "$cflags_ne10"
 fi
 
 # test of Fortran interface (by comparison with FFTW)
