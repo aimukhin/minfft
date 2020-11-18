46cd573586c0ccb432db35ac6f9306fa5e8e8713
Alexander Mukhin <alexander.i.mukhin@gmail.com>
2020-11-18 14:58:51 +0300

    mkrelease.sh: changed version number

--- tools/mkrelease.sh
+++ tools/mkrelease.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-ver=2.0.0
+ver=1.2.0
 workdir=./minfft-$ver
 
 rm -rf $workdir $workdir.tar.gz
 mkdir $workdir
-cp ../README ../LICENSE ../minfft.h ../minfft.c ../minfft.F03 $workdir
+cp ../README ../LICENSE ../minfft.{h,c,F03} $workdir
 tar cf $workdir.tar $workdir --owner=0 --group=0
 gzip -9 $workdir.tar
 echo Created $workdir.tar.gz
