bd21397636f8401985a1e621ed4edca14152505f
Alexander Mukhin <alexander.i.mukhin@gmail.com>
2020-11-30 15:07:33 +0300

    chkperf.c: added comparison

--- tools/chkperf.c
+++ tools/chkperf.c
@@ -4,6 +4,7 @@
 #include <math.h>
 #include <float.h>
 #include <sys/time.h>
+#include <tgmath.h>
 
 #define D_MAX 3
 #define T_MAX (1<<30)
@@ -20,11 +21,11 @@
 #define FFTW22(F,S) fftw##S##_##F
 
 struct {
-	int d;
-	int n[D_MAX];
-	void *in;
-	void *out;
-	void *x;
+	int d,n[D_MAX];
+	void *in1,*in2;
+	void *out1,*out2;
+	void *x1,*x2;
+	int insz,outsz;
 } P;
 
 int
@@ -63,33 +64,57 @@ alloc (void) {
 	int i,p;
 #if DFT||INVDFT
 	for (p=1,i=0; i<P.d; p*=P.n[i++]);
-	P.in=malloc(p*sizeof(minfft_cmpl));
-	P.out=malloc(p*sizeof(minfft_cmpl));
+	P.insz=2*p;
+	P.outsz=2*p;
 #elif REALDFT
 	for (p=1,i=0; i<P.d-1; p*=P.n[i++]);
-	P.in=malloc(p*P.n[P.d-1]*sizeof(minfft_real));
-	P.out=malloc(p*(P.n[P.d-1]/2+1)*sizeof(minfft_cmpl));
+	P.insz=p*P.n[P.d-1];
+	P.outsz=p*(P.n[P.d-1]+2);
 #elif INVREALDFT
 	for (p=1,i=0; i<P.d-1; p*=P.n[i++]);
-	P.in=malloc(p*(P.n[P.d-1]/2+1)*sizeof(minfft_cmpl));
-	P.out=malloc(p*P.n[P.d-1]*sizeof(minfft_real));
+	P.insz=p*(P.n[P.d-1]+2);
+	P.outsz=p*P.n[P.d-1];
 #elif DCT2||DST2||DCT3||DST3||DCT4||DST4
 	for (p=1,i=0; i<P.d; p*=P.n[i++]);
-	P.in=malloc(p*sizeof(minfft_real));
-	P.out=malloc(p*sizeof(minfft_real));
+	P.insz=p;
+	P.outsz=p;
+#endif
+	P.in1=malloc(P.insz*sizeof(minfft_real));
+	P.in2=malloc(P.insz*sizeof(minfft_real));
+	P.out1=malloc(P.outsz*sizeof(minfft_real));
+	P.out2=malloc(P.outsz*sizeof(minfft_real));
+}
+
+void
+init (void) {
+	int i;
+	minfft_real *in1=P.in1,*in2=P.in2;
+#if !INVREALDFT
+	for (i=0; i<P.insz; ++i)
+		in1[i]=in2[i]=(minfft_real)rand()/RAND_MAX-0.5;
+#else
+	// tricky
+	minfft_aux *a=minfft_mkaux_realdft(P.d,P.n);
+	minfft_real *x=P.out1;
+	for (i=0; i<P.outsz; ++i)
+		x[i]=(minfft_real)rand()/RAND_MAX-0.5;
+	minfft_realdft(x,P.in1,a);
+	minfft_free_aux(a);
+	for (i=0; i<P.insz; ++i)
+		in2[i]=in1[i];
 #endif
 }
 
 void
 setup_minfft (void) {
 #if DFT||INVDFT
-	P.x=minfft_mkaux_dft(P.d,P.n);
+	P.x1=minfft_mkaux_dft(P.d,P.n);
 #elif REALDFT||INVREALDFT
-	P.x=minfft_mkaux_realdft(P.d,P.n);
+	P.x1=minfft_mkaux_realdft(P.d,P.n);
 #elif DCT2||DST2||DCT3||DST3
-	P.x=minfft_mkaux_t2t3(P.d,P.n);
+	P.x1=minfft_mkaux_t2t3(P.d,P.n);
 #elif DCT4||DST4
-	P.x=minfft_mkaux_t4(P.d,P.n);
+	P.x1=minfft_mkaux_t4(P.d,P.n);
 #endif
 }
 
@@ -98,43 +123,43 @@ doit_minfft (int T) {
 	int t;
 	for (t=0; t<T; ++t)
 #if DFT
-		minfft_dft(P.in,P.out,P.x);
+		minfft_dft(P.in1,P.out1,P.x1);
 #elif INVDFT
-		minfft_invdft(P.in,P.out,P.x);
+		minfft_invdft(P.in1,P.out1,P.x1);
 #elif REALDFT
-		minfft_realdft(P.in,P.out,P.x);
+		minfft_realdft(P.in1,P.out1,P.x1);
 #elif INVREALDFT
-		minfft_invrealdft(P.in,P.out,P.x);
+		minfft_invrealdft(P.in1,P.out1,P.x1);
 #elif DCT2
-		minfft_dct2(P.in,P.out,P.x);
+		minfft_dct2(P.in1,P.out1,P.x1);
 #elif DST2
-		minfft_dst2(P.in,P.out,P.x);
+		minfft_dst2(P.in1,P.out1,P.x1);
 #elif DCT3
-		minfft_dct3(P.in,P.out,P.x);
+		minfft_dct3(P.in1,P.out1,P.x1);
 #elif DST3
-		minfft_dst3(P.in,P.out,P.x);
+		minfft_dst3(P.in1,P.out1,P.x1);
 #elif DCT4
-		minfft_dct4(P.in,P.out,P.x);
+		minfft_dct4(P.in1,P.out1,P.x1);
 #elif DST4
-		minfft_dst4(P.in,P.out,P.x);
+		minfft_dst4(P.in1,P.out1,P.x1);
 #endif
 }
 
 void
 cleanup_minfft (void) {
-	minfft_free_aux(P.x);
+	minfft_free_aux(P.x1);
 }
 
 void
 setup_fftw (void) {
 #if DFT
-	P.x=FFTW(plan_dft)(P.d,P.n,P.in,P.out,FFTW_FORWARD,FFTW_ESTIMATE);
+	P.x2=FFTW(plan_dft)(P.d,P.n,P.in2,P.out2,FFTW_FORWARD,FFTW_ESTIMATE);
 #elif INVDFT
-	P.x=FFTW(plan_dft)(P.d,P.n,P.in,P.out,FFTW_BACKWARD,FFTW_ESTIMATE);
+	P.x2=FFTW(plan_dft)(P.d,P.n,P.in2,P.out2,FFTW_BACKWARD,FFTW_ESTIMATE);
 #elif REALDFT
-	P.x=FFTW(plan_dft_r2c)(P.d,P.n,P.in,P.out,FFTW_ESTIMATE);
+	P.x2=FFTW(plan_dft_r2c)(P.d,P.n,P.in2,P.out2,FFTW_ESTIMATE);
 #elif INVREALDFT
-	P.x=FFTW(plan_dft_c2r)(P.d,P.n,P.in,P.out,FFTW_ESTIMATE);
+	P.x2=FFTW(plan_dft_c2r)(P.d,P.n,P.in2,P.out2,FFTW_ESTIMATE);
 #else
 	fftw_r2r_kind kinds[D_MAX];
 	for (int i=0; i<P.d; ++i)
@@ -151,7 +176,7 @@ setup_fftw (void) {
 #elif DST4
 		kinds[i]=FFTW_RODFT11;
 #endif
-	P.x=FFTW(plan_r2r)(P.d,P.n,P.in,P.out,kinds,FFTW_ESTIMATE);
+	P.x2=FFTW(plan_r2r)(P.d,P.n,P.in2,P.out2,kinds,FFTW_ESTIMATE);
 #endif
 }
 
@@ -159,18 +184,40 @@ void
 doit_fftw (int T) {
 	int t;
 	for (t=0; t<T; ++t)
-		FFTW(execute)(P.x);
+		FFTW(execute)(P.x2);
 }
 
 void
 cleanup_fftw (void) {
-	FFTW(destroy_plan)(P.x);
+	FFTW(destroy_plan)(P.x2);
 }
 
 void
 dealloc (void) {
-	free(P.in);
-	free(P.out);
+	free(P.in1);
+	free(P.in2);
+	free(P.out1);
+	free(P.out2);
+}
+
+double
+cmp (void) {
+	double d,v,dmax,vmax;
+	minfft_real *o1,*o2;
+	int i;
+	doit_minfft(1);
+	doit_fftw(1);
+	dmax=0;
+	vmax=0;
+	o1=P.out1;
+	o2=P.out2;
+	for (i=0; i<P.outsz; ++i) {
+		d=fabs(o1[i]-o2[i]);
+		dmax=(d>dmax)?d:dmax;
+		v=fabs(o2[i]);
+		vmax=(v>vmax)?v:vmax;
+	}
+	return dmax/vmax;
 }
 
 int
@@ -186,12 +233,11 @@ mflops (double us) {
 }
 
 int
-perf (void(*setup)(void), void(*doit)(int), void(*cleanup)(void)) {
+perf (void(*doit)(int)) {
 	int T;
 	struct timeval t1,t2;
 	double us;
 	int p=-1;
-	(*setup)();
 	for (T=1; T<T_MAX; T*=2) {
 		gettimeofday(&t1,NULL);
 		(*doit)(T);
@@ -202,23 +248,27 @@ perf (void(*setup)(void), void(*doit)(int), void(*cleanup)(void)) {
 			break;
 		}
 	}
-	(*cleanup)();
 	return p;
 }
 
 int
 main (int argc, char **argv) {
+	double r;
 	int p1,p2;
 	if (argc!=2)
 		return 1;
 	if (parse(argv[1])!=0)
 		return 2;
 	alloc();
-	p1=perf(setup_minfft,doit_minfft,cleanup_minfft);
-	p2=perf(setup_fftw,doit_fftw,cleanup_fftw);
+	init();
+	setup_minfft();
+	setup_fftw();
+	r=cmp();
+	p1=perf(doit_minfft);
+	p2=perf(doit_fftw);
+	cleanup_minfft();
+	cleanup_fftw();
 	dealloc();
-	if (p1<0||p2<0)
-		return 3;
-	printf("%d %d\n",p1,p2);
+	printf("%g %d %d\n",r,p1,p2);
 	return 0;
 }
