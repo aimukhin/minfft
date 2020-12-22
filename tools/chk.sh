a4d7f7c857c9001b2e404f99fa6da3cac6b8e20f
Alexander Mukhin <alexander.i.mukhin@gmail.com>
2020-12-22 14:29:15 +0300

    chk.sh: pick CC from environment

--- tools/chk.sh
+++ tools/chk.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # init
+if [ -z $CC ]; then CC=cc; fi
 CFLAGS="$CFLAGS -std=c99 -pedantic -Wall -Wextra"
 CFLAGS="$CFLAGS $(echo -I.. -I ~/build/fftw/include/ -L ~/build/fftw/lib/)"
 LIBS=-lm
@@ -25,13 +26,13 @@ else
 fi
 
 # build minfft
-cc $DEFS $CFLAGS -c ../minfft.c
+$CC $DEFS $CFLAGS -c ../minfft.c
 
 # run tests
 XFORMS="DFT INVDFT REALDFT INVREALDFT DCT2 DST2 DCT3 DST3 DCT4 DST4"
 SIZES="2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2x1 4x2 8x4 16x8 32x16 64x32 128x64 256x128 512x256 1024x512 4x2x1 8x4x2 16x8x4 32x16x8 64x32x16 128x64x32 8x4x2x1 16x8x4x2 32x16x8x4 64x32x16x8"
 for x in $XFORMS; do
-	cc -D$x $DEFS $CFLAGS chk.c minfft.o $LIBS
+	$CC -D$x $DEFS $CFLAGS chk.c minfft.o $LIBS
 	if [ $? -ne 0 ]; then
 		exit 1
 	fi
